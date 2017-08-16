b0f6e8d883ccd33b4112c5968565dcc6758b725c
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-16 19:34:43 -0400

    add show route

--- js/controllers/song.controller.js
+++ js/controllers/song.controller.js
@@ -4,7 +4,15 @@ angular
   'Song',
   SongIndexControllerFunction
 ])
+.controller('SongShowController', [
+  'Song',
+  '$stateParams',
+  SongShowControllerFunction
+])
 
 function SongIndexControllerFunction (Song) {
   this.songs = Song.query()
+}
+function SongShowControllerFunction (Song, $stateParams) {
+  this.song = Song.get({ id: $stateParams.id })
 }
--- js/song.routes.js
+++ js/song.routes.js
@@ -13,7 +13,11 @@ function Router ($stateProvider) {
       controller: 'SongIndexController',
       controllerAs: 'vm',
       templateUrl: 'js/ng-views/index.html'
-
     })
-  // $urlRouterProvider.otherwise('/songs')
+    .state('songShow', {
+      url: '/songs/:id',
+      controller: 'SongShowController',
+      controllerAs: 'vm',
+      templateUrl: 'js/ng-views/show.html'
+    })
 }
